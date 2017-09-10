42a82962a7d187cbef3af9e06259ac7909a63bc6
Nick <nik9993@gmail.com>
2017-09-10 16:16:04 -0400

    Changed the comment to look more professional and added a copyright  Changes to be committed: 	modified:   Client/Nimbus-One.py 	modified:   Server/Nimbus-One_serv.py

--- Server/Nimbus-One_serv.py
+++ Server/Nimbus-One_serv.py
@@ -1,12 +1,31 @@
-#Coding Guidelines:
-# The PEP 8 Style Guide found here:
-#    https://www.python.org/dev/peps/pep-0008/
-#
-# Please Adhere to these guidelines while coding! It is very important that we conform to a convention.
-# If you have an issue with the guidelines then please bring it up and we might change them,
-# But for now Follow These Rules EVERYWHERE you Code!
-#
-# String Quotes:
-# 1. Single single quotes will be used for small symbol-like strings.
-# 2. Double double quotes will be used for string that are used for interpolation or that are natural language messages.
-# 3. Triple double quotes are for docstrings and raw string literals. 
+#!/usr/bin/python3
+#*****
+#* @(#)Nimbus-One_serve.py
+#*
+#* Nimbus One Service Launcher
+#* 2017(c)Copyright
+#*
+#* Contributors: 
+#*   Allan "Nick" Pedrana
+#*
+#* @version 0.01 9/10/2017
+#*  Alpha Realease Version
+#*
+#*****
+#* Program History:
+#*  -9/10/2017: File Created
+#*
+#*****
+#*Coding Guidelines:
+#* The PEP 8 Style Guide found here:
+#*    https://www.python.org/dev/peps/pep-0008/
+#*
+#* Please Adhere to these guidelines while coding! It is very important that we conform to a convention.
+#* If you have an issue with the guidelines then please bring it up and we might change them,
+#* But for now Follow These Rules EVERYWHERE you Code!
+#*
+#* String Quotes:
+#* 1. Single single quotes will be used for small symbol-like strings.
+#* 2. Double double quotes will be used for string that are used for interpolation or that are natural language messages.
+#* 3. Triple double quotes are for docstrings and raw string literals. 
+#*****
