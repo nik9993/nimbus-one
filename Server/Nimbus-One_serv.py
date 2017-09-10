9a12390b71b0cf5a30361f13034f2779f7cc401f
Nick <nik9993@gmail.com>
2017-09-10 15:54:29 -0400

    Changes to be committed: 	renamed:    Nimbus-One.py -> Client/Nimbus-One.py 	new file:   Server/Nimbus-One_serv.py

---
+++ Server/Nimbus-One_serv.py
@@ -0,0 +1,12 @@
+#Coding Guidelines:
+# The PEP 8 Style Guide found here:
+#    https://www.python.org/dev/peps/pep-0008/
+#
+# Please Adhere to these guidelines while coding! It is very important that we conform to a convention.
+# If you have an issue with the guidelines then please bring it up and we might change them,
+# But for now Follow These Rules EVERYWHERE you Code!
+#
+# String Quotes:
+# 1. Triple double quotes are for docstrings and raw string literals. 
+# 2. Double double quotes will be used for string that are used for interpolation or that are natural language messages.
+# 3. Single double quotes will be used for small symbol-like strings.
