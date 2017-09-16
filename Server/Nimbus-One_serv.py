042a49d44c12d6b7ad3666af50c009d6850be5b5
Jonathan Salazar <jonathansalazar94@gmail.com>
2017-09-16 13:50:54 -0500

    Split files

--- Server/Nimbus-One_serv.py
+++ Server/Nimbus-One_serv.py
@@ -32,8 +32,9 @@
 #* 5. ObjNames should be CamelCase and be an abbreviation of the Class it came from
 #*      Example: 
 #*         Class Name: PollingServer, Object Name: PollServ
+#* 6. filenames should have short, all-lowercase names, and they can contain underscores
+#* 7. packages should have short, all-lowercase names, preferably without underscores
 #* 
-#*
 #* String Quotes:
 #* 1. Single single quotes will be used for small symbol-like strings.
 #* 2. Double double quotes will be used for string that are used for interpolation or that are natural language messages.
@@ -44,43 +45,6 @@ import threading
 
 PROG_NAME_V = "Nimbus One Server v0.01"
 PROG_NAME   = "Nimbus One Server"
-
-
-##################################################################
-#                       Client Thread                            #
-##################################################################
-#*****
-#* Purpose: This thread will be created everytime a new client
-#*           send a discovery packet to the polling server
-#*****
-
-class Client(threading.Thread):
-    """The Thread that initiates once a client connects to the server"""
-    def __init__(self, address):
-        threading.Thread.__init__(self)
-        self.address = address
-    def run(self):
-        print("Hello, Client!")
-
-##################################################################
-#                    Polling Server Thread                       #
-##################################################################
-#*****
-#* Purpose: This is the polling server thread that will recieve
-#*           all of the discovery packets from the clients
-#*****
-
-class PollingServer(threading.Thread):
-    """This Thread is the Main Polling Thread of the Server"""
-    def __init__(self, port):
-        threading.Thread.__init__(self)
-        self.portNumber = port
-
-    def run(self):
-        try:
-            print("Hello, World!")
-        except KeyboardInterrupt:
-            print ("^C received, shutting down the polling server")
 
 ##################################################################
 #                     MAIN Program Flow                          #
---
+++ Server/pollingserverthread.py
@@ -0,0 +1,19 @@
+##################################################################
+#                    Polling Server Thread                       #
+##################################################################
+#*****
+#* Purpose: This is the polling server thread that will recieve
+#*           all of the discovery packets from the clients
+#*****
+
+class PollingServer(threading.Thread):
+    """This Thread is the Main Polling Thread of the Server"""
+    def __init__(self, port):
+        threading.Thread.__init__(self)
+        self.portNumber = port
+
+    def run(self):
+        try:
+            print("Hello, World!")
+        except KeyboardInterrupt:
+            print ("^C received, shutting down the polling server")
