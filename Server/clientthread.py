4235f572ab11547c229325e9d63a1314820344e9
Jonathan Salazar <jonathansalazar94@gmail.com>
2017-09-16 15:26:35 -0500

    Added Client Server

--- Server/clientthread.py
+++ Server/clientthread.py
@@ -9,8 +9,35 @@ import threading
 
 class Client(threading.Thread):
     """The Thread that initiates once a client connects to the server"""
-    def __init__(self, address):
+    def __init__(self,(conn, address):
         threading.Thread.__init__(self)
+        self.client_conn = conn
         self.address = address
+        self.connected = 1 
+        self.running = 1
+        self.size = 0
+        
     def run(self):
+        while running:
+			if self.connected == 1:
+				try:
+					data =  self.client_conn.recv(self.size);
+					self.client_arg_parcer(data);
+				except socket.error, (errCode, Message):
+					if errCode == 10035 or errCode == 11:
+						#Nothing wrong, this is non blocking error. Continue to check fordata in.
+						#self.get_data();
+						pass
+					else:
+						self.client.send("There has been an error recieving your data, please dissconnect and try again.\r\n");
+						print("There has been an error recieving your data, please dissconnect and try again.\r\n");
+						print("Error Code: " + str(errCode));
+						print("Message: " + str(Message) + "\r\n");
+			else:
+				if self.con == 1:
+					data = self.client.recv(self.size);
+					self.client_arg_parcer(data);
+				#else: must end thread here
+				
+			time.sleep(0.1);
         print("Hello, Client!")
--- Server/pollingserverthread.py
+++ Server/pollingserverthread.py
@@ -7,6 +7,7 @@
 #*****
 from clientthread import Client
 import threading
+import socket
 
 threads = []
 
@@ -15,13 +16,34 @@ class PollingServer(threading.Thread):
     def __init__(self, port):
         threading.Thread.__init__(self)
         self.portNumber = port
+    
+    def open_socket(self):
+		"""Function called once a clients connects to the host on correct socket."""
+		try:
+			self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.server.bind((self.host, self.port))
+			self.server.listen(5)
+			print "Polling Server Listening for connection..."
+		except socket.error, (value, message):
+			if self.server:
+				self.server.close()
+			print("Could not open Server Socket: " + message)
+			print("Please try again")
+			sys.exit(1)
+			return
 
     def run(self):
+    poll_sock = self.open_socket()
+    poll_sock.bind((host, port))
+    
+    poll_sock.listen(1)
         try:
             print("Hello, World!")
-            c = Client()
-            c.start()
-            threads.append(c)
+                while 1:
+                conn = poll_sock.server.accept()
+                c = Client(conn)
+                c.start()
+                threads.append(c)
 			
         except KeyboardInterrupt:
             print ("^C received, shutting down the polling server")
