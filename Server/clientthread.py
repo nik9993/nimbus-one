a42d4bd81a2d6562d82a9a980e0a0c8b68cb853b
Jonathan Salazar <jonathansalazar94@gmail.com>
2017-09-16 15:32:30 -0500

    Fix for dev run

--- Server/clientthread.py
+++ Server/clientthread.py
@@ -22,22 +22,21 @@ class Client(threading.Thread):
 			if self.connected == 1:
 				try:
 					data =  self.client_conn.recv(self.size);
-					self.client_arg_parcer(data);
+#				self.client_arg_parcer(data);
 				except socket.error, (errCode, Message):
 					if errCode == 10035 or errCode == 11:
 						#Nothing wrong, this is non blocking error. Continue to check fordata in.
 						#self.get_data();
 						pass
 					else:
-						self.client.send("There has been an error recieving your data, please dissconnect and try again.\r\n");
+						self.client_conn.send("There has been an error recieving your data, please dissconnect and try again.\r\n");
 						print("There has been an error recieving your data, please dissconnect and try again.\r\n");
 						print("Error Code: " + str(errCode));
 						print("Message: " + str(Message) + "\r\n");
 			else:
 				if self.con == 1:
-					data = self.client.recv(self.size);
-					self.client_arg_parcer(data);
+					data = self.client_conn.recv(self.size);
+#				self.client_arg_parcer(data);
 				#else: must end thread here
 				
 			time.sleep(0.1);
-        print("Hello, Client!")
